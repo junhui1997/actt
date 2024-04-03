b8ef8d5b05f02a4456cb88213184e8451ee6cb79
u <jh4165@columbia.edu>
2024-04-03 22:44:42 +0800

    change mini

--- scripts/generate_surgical_dataset.sh
+++ scripts/generate_surgical_dataset.sh
@@ -1,5 +1,5 @@
 cd ..
-for task_name in PegTransfer-v0 BiPegTransfer-v0 NeedleReach-v0
+for task_name in NeedleRegrasp-v0 PegTransfer-v0 BiPegTransfer-v0 NeedleReach-v0
 do
 python -u record_surgical_episodes_joint.py \
 --task_name $task_name --dataset_dir dataset_m/$task_name-joint/ --num_episodes 100
