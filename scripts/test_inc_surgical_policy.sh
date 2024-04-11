5e378dc0ef690ecbc5b45c0498a0051cde06ef06
u <jh4165@columbia.edu>
2024-04-11 16:12:25 +0800

    add script

--- scripts/test_inc_surgical_policy.sh
+++ scripts/test_inc_surgical_policy.sh
@@ -2,31 +2,43 @@
 cd ..
 #for model in ACT
 #do
-#  for task_name in  NeedleRegrasp-v0
-#do
-#  for chunk_size in  20
+#  for task_name in NeedleRegrasp-v0
 #do
 #python -u imitate_episodes.py \
-#--task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name/ --policy_class $model --kl_weight 200 --chunk_size $chunk_size --hidden_dim 512 --batch_size 6 --dim_feedforward 3200 --state_dim 14 --action_dim 14 --num_steps 100000 --lr 75e-7 --seed 0 --is_surgical  --eval_every 2500 --save_every 5000 --temporal_agg
-#done
-#done
-#done
-
-#for model in ACT
-#do
-#  for task_name in NeedleRegrasp-v0  BiPegTransfer-v0
-#do
-#python -u imitate_episodes.py \
-#--task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name/ --policy_class $model --kl_weight 200 --chunk_size 40 --hidden_dim 512 --batch_size 6 --dim_feedforward 3200 --state_dim 14 --action_dim 10 --num_steps 100000 --lr 75e-7 --seed 0 --is_surgical  --eval_every 2500 --save_every 5000 --temporal_agg
+#--task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name-inc/ --policy_class $model --kl_weight 200 --chunk_size 40 --hidden_dim 512 --batch_size 6 --dim_feedforward 3200 --state_dim 14 --action_dim 10 --num_steps 100000 --lr 75e-7 --seed 0 --is_surgical  --eval_every 2500 --save_every 5000 --temporal_agg
 #done
 #done
 
 # single arm
 for model in ACT
 do
-  for task_name in  NeedlePick-v0  PegTransfer-v0 NeedleReach-v0
+  for task_name in  PegTransfer-v0
 do
 python -u imitate_episodes.py \
 --task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name-inc/ --policy_class $model --kl_weight 200 --chunk_size 20 --hidden_dim 512 --batch_size 6 --dim_feedforward 3200 --state_dim 7 --action_dim 5 --num_steps 100000 --lr 75e-7 --seed 0 --is_surgical  --eval_every 2500 --save_every 5000 --temporal_agg
 done
 done
+
+for model in ACT
+do
+  for task_name in  NeedleReach-v0
+do
+python -u imitate_episodes.py \
+--task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name-inc/ --policy_class $model --kl_weight 200 --chunk_size 20 --hidden_dim 512 --batch_size 6 --dim_feedforward 3200 --state_dim 7 --action_dim 5 --num_steps 10000 --lr 75e-7 --seed 0 --is_surgical  --eval_every 2500 --save_every 5000 --temporal_agg
+done
+done
+
+
+for model in ACT
+do
+  for task_name in  BiPegTransfer-v0
+do
+  for chunk_size in  40
+do
+python -u imitate_episodes.py \
+--task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name-inc/ --policy_class $model --kl_weight 200 --chunk_size $chunk_size --hidden_dim 512 --batch_size 6 --dim_feedforward 3200 --state_dim 14 --action_dim 14 --num_steps 100000 --lr 75e-7 --seed 0 --is_surgical  --eval_every 2500 --save_every 5000 --temporal_agg
+done
+done
+done
+
+
---
+++ scripts/test_inc_surgical_policy2.sh
@@ -0,0 +1,46 @@
+#conda activate surrol_aloha
+cd ..
+# dual arm short time and chunk size
+for model in Diffusion
+do
+  for task_name in NeedleRegrasp-v0
+do
+python -u imitate_episodes.py \
+--task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name-inc/ --policy_class $model --kl_weight 200 --chunk_size 20 --hidden_dim 512 --batch_size 12 --dim_feedforward 3200 --state_dim 14 --action_dim 10 --num_steps 50000 --lr 15e-6 --seed 0 --is_surgical  --eval_every 1250 --save_every 2500 --temporal_agg
+done
+done
+
+# single arm long time
+for model in Diffusion
+do
+  for task_name in  PegTransfer-v0 NeedlePick-v0
+do
+python -u imitate_episodes.py \
+--task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name-inc/ --policy_class $model --kl_weight 200 --chunk_size 20 --hidden_dim 512 --batch_size 12 --dim_feedforward 3200 --state_dim 7 --action_dim 5 --num_steps 50000 --lr 15e-6 --seed 0 --is_surgical  --eval_every 1250 --save_every 2500 --temporal_agg
+done
+done
+
+# single arm short time
+for model in Diffusion
+do
+  for task_name in  NeedleReach-v0
+do
+python -u imitate_episodes.py \
+--task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name-inc/ --policy_class $model --kl_weight 200 --chunk_size 20 --hidden_dim 512 --batch_size 12 --dim_feedforward 3200 --state_dim 7 --action_dim 5 --num_steps 5000 --lr 15e-6 --seed 0 --is_surgical  --eval_every 1250 --save_every 2500 --temporal_agg
+done
+done
+
+# dual arm long time and chunk_size
+for model in Diffusion
+do
+  for task_name in  BiPegTransfer-v0
+do
+  for chunk_size in  40
+do
+python -u imitate_episodes.py \
+--task_name $task_name --ckpt_dir checkpoint_m/$model/$task_name-inc/ --policy_class $model --kl_weight 200 --chunk_size $chunk_size --hidden_dim 512 --batch_size 12 --dim_feedforward 3200 --state_dim 14 --action_dim 14 --num_steps 50000 --lr 15e-6 --seed 0 --is_surgical  --eval_every 1250 --save_every 2500 --temporal_agg
+done
+done
+done
+
+
